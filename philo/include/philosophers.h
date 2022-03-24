7df6a59574aa7c88db5185f640391b969c64a8b1
HolyJack <holyjackdev@gmail.com>
2022-03-24 21:21:39 +0300

    include added

--- philo/include/philosophers.h
+++ philo/include/philosophers.h
@@ -6,11 +6,13 @@
 /*   By: ejafer <ejafer@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/24 21:02:04 by ejafer            #+#    #+#             */
-/*   Updated: 2022/03/24 21:05:29 by ejafer           ###   ########.fr       */
+/*   Updated: 2022/03/24 21:19:19 by ejafer           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PHILOSOPHERS_H
 # define PHILOSOPHERS_H
-
+# include <pthread.h>
+# include <stdlib.h>
+# include <stdio.h>
 #endif
